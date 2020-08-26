f4d1cf1e0cc87afab85ece1dd6ced3891df9cd42
mrdanielmh <57440223+mrdanielmh@users.noreply.github.com>
2020-08-26 09:48:17 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ module "vpc" {
   name = "showcase-demo-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-west-2a", "us-west-2b"]
+  azs             = ["us-east-2a", "us-east-2b"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
